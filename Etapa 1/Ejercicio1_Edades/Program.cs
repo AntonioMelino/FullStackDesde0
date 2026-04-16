afc74a1ee424aa88362852356f7ff2a92b6ab43f
AntonioMelino <antoniomelino1997@gmail.com>
2026-04-15 23:22:57 -0300

    Initial commit

---
+++ Etapa 1/Ejercicio1_Edades/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Ingrese su edad: ");
+int edad = int.Parse(Console.ReadLine());
+if (edad < 13)
+{
+    Console.WriteLine("Niño");
+    int dieciocho = 18 - edad;
+    Console.WriteLine($"Para los 18 faltan {dieciocho} años");
+}else if(edad >= 13 && edad <= 17)
+{
+    Console.WriteLine("Adolescente");
+    int dieciocho = 18 - edad;
+    Console.WriteLine($"Para los 18 faltan {dieciocho} años");
+}
+else if(edad >= 18 &&  edad <= 64)
+{
+    Console.WriteLine("Adulto");
+    int dieciocho = edad - 18;
+    Console.WriteLine($"Para los 18 pasaron {dieciocho} años");
+}
+else
+{
+    Console.WriteLine("Adulto mayor");
+    int dieciocho = edad - 18;
+    Console.WriteLine($"Para los 18 pasaron {dieciocho} años");
+}
+
---
+++ Etapa 1/MiPrimerApp/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Hola, soy Antonio!");
+Console.WriteLine("Estoy aprendiendo C# con .NET 8");
+Console.Write("¿Cuál es tu nombre? ");
+string nombre = Console.ReadLine();
+Console.WriteLine($"Bienvenido, {nombre}!");
+Console.ReadKey();
